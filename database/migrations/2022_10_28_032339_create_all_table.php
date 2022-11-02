e4a8d943bb31112fdf3e9ae9b6392a2a32eaf622
tanyogene <tanyogene@gmail.com>
2022-11-02 19:59:46 +0800

    #

--- database/migrations/2022_10_28_032339_create_all_table.php
+++ database/migrations/2022_10_28_032339_create_all_table.php
@@ -90,7 +90,7 @@ return new class extends Migration
             $table->string('post_id')->primary();
             $table->string('title');
             $table->mediumText('description');
-            $table->integer('room_size');
+            $table->string('room_size');
             $table->text('address');
             $table->string('condominium_name')->unique();
             $table->string('block');
@@ -138,8 +138,7 @@ return new class extends Migration
 
         Schema::create('visit_appointments', function (Blueprint $table) {
             $table->string('appointment_id')->primary();
-            $table->date('date');
-            $table->dateTime('time');
+            $table->dateTime('datetime');
             $table->text('note');
             $table->string('status');
             $table->string('post_id');
@@ -181,7 +180,7 @@ return new class extends Migration
             $table->string('group_message_id')->primary();
             $table->text('message');
             $table->string('sender_id');
-            $table->string('chat_id');
+            $table->string('group_id');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
@@ -303,7 +302,7 @@ return new class extends Migration
 
         Schema::table('group_messages', function (Blueprint $table) {
             $table->foreign('sender_id')->references('account_id')->on('accounts');
-            $table->foreign('chat_id')->references('group_id')->on('group_chats');
+            $table->foreign('group_id')->references('group_id')->on('group_chats');
         });
 
         Schema::table('group_users', function (Blueprint $table) {
