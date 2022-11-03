87ede39085e3d1415a882238e744a4d7e4531096
Xande Yong <7358607-Xande99@users.noreply.gitlab.com>
2022-11-03 16:40:06 +0800

    update dashboard design

--- resources/views/dashboard/dashboard_profile.blade.php
+++ resources/views/dashboard/dashboard_profile.blade.php
@@ -13,113 +13,148 @@
 
     <body>
 
-
         @include('../base/dashboard/dashboard_sidebar')
 
         <div id="wrapper">
             <div class="container-fluid">
                 @include('../base/dashboard/dashboard_header')
 
-                <div id="content" class="row">
+                <div id="content" class="row justify-content-center">
 
                     {{-- Code here --}}
-                    <div class="col justify-content-center">
+                    <div class="col col-sm-10 col-md-8 col-lg-10">
 
                         {{-- Profile Details --}}
                         <div class="container">
+                            <div class="row row-gap align-items-center">
 
-                            <div class="row row-gap">
-                                <img src="/image/condo.webp" class="img-fluid img-thumbnail rounded float-left"
-                                    alt="...">
+                                <div class="col-12 col-lg-4 mb-3 mb-lg-0">
+                                    <img src="/image/condo.webp" class="img-fluid img-thumbnail rounded" alt="...">
+                                </div>
 
-                                <div class="row row-gap">
-                                    <div class="col-md-2">
-                                        <label>ID :</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label>gagagag</label>
+                                <div class="col-12 col-lg-8">
+                                    <div class="card">
+
+                                        <div class="card-header">
+                                            <h5>Information</h5>
+                                        </div>
+
+                                        <ul class="list-group list-group-flush">
+                                            <li class="list-group-item">
+                                                <div class="row">
+                                                    <div class="col col-lg-3">
+                                                        <label>ID :</label>
+                                                    </div>
+                                                    <div class="col col-lg-9">
+                                                        <label>A1</label>
+                                                    </div>
+                                                </div>
+                                            </li>
+
+                                            <li class="list-group-item">
+                                                <div class="row">
+                                                    <div class="col col-lg-3">
+                                                        <label>Name :</label>
+                                                    </div>
+                                                    <div class="col col-lg-9">
+                                                        <label>Genji</label>
+                                                    </div>
+                                                </div>  
+                                            </li>
+
+                                            <li class="list-group-item">
+                                                <div class="row">
+                                                    <div class="col col-lg-3">
+                                                        <label>Gender :</label>
+                                                    </div>
+                                                    <div class="col col-lg-9">
+                                                        <label>Male</label>
+                                                    </div>
+                                                </div>  
+                                            </li>
+
+                                            <li class="list-group-item">
+                                                <div class="row">
+                                                    <div class="col col-lg-3">
+                                                        <label>Age :</label>
+                                                    </div>
+                                                    <div class="col col-lg-9">
+                                                        <label>28</label>
+                                                    </div>
+                                                </div>  
+                                            </li>
+
+                                            <li class="list-group-item">
+                                                <div class="row">
+                                                    <div class="col col-lg-3">
+                                                        <label>Phone Number :</label>
+                                                    </div>
+                                                    <div class="col col-lg-9">
+                                                        <label>0137897232</label>
+                                                    </div>
+                                                </div>  
+                                            </li>
+
+                                            <li class="list-group-item">
+                                                <div class="row">
+                                                    <div class="col col-lg-3">
+                                                        <label>Email :</label>
+                                                    </div>
+                                                    <div class="col col-lg-9">
+                                                        <label>genji@battle.net</label>
+                                                    </div>
+                                                </div>  
+                                            </li>
+                                        </ul>
+
                                     </div>
                                 </div>
-                                <div class="row row-gap">
-                                    <div class="col-md-2">
-                                        <label>Name :</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label>agggg</label>
-                                    </div>
-                                </div>
-                                <div class="row row-gap">
-                                    <div class="col-md-2">
-                                        <label>Gender :</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label>agggg</label>
-                                    </div>
-                                </div>
-                                <div class="row row-gap">
-                                    <div class="col-md-2">
-                                        <label>Age :</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label>agggg</label>
-                                    </div>
-                                </div>
-                                <div class="row row-gap">
-                                    <div class="col-md-2">
-                                        <label>Phone Number :</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label>agggg</label>
-                                    </div>
-                                </div>
-                                <div class="row row-gap">
-                                    <div class="col-md-2">
-                                        <label>Email :</label>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label>agggg</label>
-                                    </div>
-                                </div>
+
                             </div>
 
-                            {{-- Button to Edit Profile --}}
-                            <br>
-                            <div class="row">
-                                <button type="button" class="edit-profile-button">Edit Profile</button>
+                            <div id="edit_profile" class="row row-gap justify-content-center mt-1">
+                                <div class="col text-center">
+                                    <button type="button" class="btn btn-lg btn-light btn-outline-dark w-100">Edit Profile</button>
+                                </div>
                             </div>
                         </div>
 
-                        <br>
-                        {{-- Change Password Form --}}
-                        <div class="container change-password-container">
-                            <form action="./backend/profile.php" method="post">
-                                <div class="row change-password-title">
-                                        <h2>Change Password</h2>  
-                                </div>
 
-                                <div class="row row-gap">
-                                    <div class="col-md-2">
-                                        <label>New Password :</label>
-                                    </div>
-                                    <div class="col-md">
-                                        <input type="text" name="username" class="w-100" >
-                                    </div>
-                                </div>
-                                <div class="row row-gap">
-                                    <div class="col-md-2">
-                                        <label>Old Password :</label>
-                                    </div>
-                                    <div class="col-md">
-                                        <input type="password" name="password" class="w-100" >
-                                    </div>
-                                </div>
-                                <div class="row">   
-                                    <div style="text-align: center;">     
-                                        <input type="submit" name="Change Password" value="Change Password" class="col-md-4 change-password-button">
-                                    </div>
-                                </div>
+                        {{-- Change Password --}}
+                        <div id="change_password" class="container mt-4">
+                            <div class="row">
+                                <div class="col">
 
-                            </form>
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <h5>Change Password</h5>
+                                        </div>
+
+                                        <div class="card-body">
+                                            <div class="row row-gap">
+                                                <div class="col-12 col-lg-3">
+                                                    <label>New Password: </label>
+                                                </div>
+                                                <div class="col-12 col-lg-9">
+                                                    <input class="form-control" type="text">
+                                                </div>
+                                            </div>
+
+                                            <div class="row row-gap">
+                                                <div class="col-12 col-lg-3">
+                                                    <label>Old Password: </label>
+                                                </div>
+                                                <div class="col-12 col-lg-9">
+                                                    <input class="form-control" type="text">
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <button class="card-footer btn btn-outline-danger w-100">Change Password</button>
+                                    </div>
+
+                                </div>
+                            </div>
                         </div>
 
                     </div>
