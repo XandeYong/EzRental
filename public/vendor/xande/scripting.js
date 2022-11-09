682725e4ee3f4c20a178ca7421a890dcfbf928c8
Xande Yong <7358607-Xande99@users.noreply.gitlab.com>
2022-11-09 18:46:26 +0800

    #

--- public/vendor/xande/scripting.js
+++ public/vendor/xande/scripting.js
@@ -38,12 +38,3 @@ $(document).ready(function () {
     })
 
 });
-
-// document.getElementById('MyImage').onchange = function(e) {
-//     // Get the first file in the FileList object
-//     var imageFile = this.files[0];
-//     // get a local URL representation of the image blob
-//     var url = window.URL.createObjectURL(imageFile);
-//     // Now use your newly created URL!
-//     someImageTag.src = url;
-// }
